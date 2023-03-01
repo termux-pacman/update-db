9e71d1e5301bb7418604e32ec71108e24ce9444d
Maxython <mixython@gmail.com>
2023-03-01 14:46:13 +0300

    update main.sh

--- main.sh
+++ main.sh
@@ -21,34 +21,36 @@ done
 files=$(aws s3api list-objects --bucket "${bucket}" --prefix "${repo}/${arch}/" | jq -r '.Contents[].Key')
 sfpu_files=$(aws s3api list-objects --bucket "${SFPU}" --prefix "${repo}/${arch}/" | jq -r '.Contents[].Key')
 
-# Delete packages and sig of packages + creare new sigs
+# Delete packages and sig of packages
 case $repo in
   main|root|x11) name_fdp="deleted_termux-${repo}_packages.txt";;
   *) name_fdp="deleted_${repo}_packages.txt";;
 esac
-file_dp=$(echo "$sfpu_files" | grep "$name_fdp" | head -1)
-if [[ -n $file_dp ]]; then
-  bucket="$SFPU" get-object $file_dp $name_fdp
-  bucket="$SFPU" get-object $file_dp.sig $name_fdp.sig
-  if $(gpg --verify $name_fdp.sig $name_fdp); then
-    for i in $(cat $name_fdp); do
-      repo-remove $repo.db.tar.gz $i || true
-      del-all-pkg $(echo $i | sed 's/+/0/g')
-    done
-    bucket="$SFPU" aws-rm $file_dp
-    bucket="$SFPU" aws-rm $file_dp.sig
-    for i in db files; do
-      rm $repo.$i.tar.gz.sig
-      gpg --batch --pinentry-mode=loopback --passphrase $PW_GPG --detach-sign --use-agent -u $KEY_GPG --no-armor "$repo.$i.tar.gz"
-    done
-    upload=true
-  else
-    echo "Attention: package removal failed, sig did not match."
-  fi
-  rm $name_fdp*
+files_dp=$(echo "$sfpu_files" | grep "$name_fdp" | head -1)
+if [[ -n $files_dp ]]; then
+  for i in $files_dp; do
+    if [[ $i != *"$name_fdp"*".sig" ]]; then
+      bucket="$SFPU" get-object $i $name_fdp
+      bucket="$SFPU" get-object $i.sig $name_fdp.sig
+      if $(gpg --verify $name_fdp.sig $name_fdp); then
+        for j in $(cat $name_fdp); do
+          for z in "" "-static"; do
+            repo-remove $repo.db.tar.gz "${j}${z}" || true
+            del-all-pkg $(echo "${j}${z}" | sed 's/+/0/g')
+          done
+        done
+        bucket="$SFPU" aws-rm $file_dp
+        bucket="$SFPU" aws-rm $file_dp.sig
+        upload=true
+      else
+        echo "Attention: package removal failed, sig did not match."
+      fi
+      rm $name_fdp*
+    fi
+  done
 fi
 
-# Update packages and create new sigs
+# Update packages and create new sigs of packages
 files_pkg=$(echo "$sfpu_files" | grep "\.pkg\.")
 if [[ -n $files_pkg ]]; then
   for i in $files_pkg; do
@@ -61,8 +63,13 @@ if [[ -n $files_pkg ]]; then
         bucket="$SFPU" aws-rm $i
         bucket="$SFPU" aws-rm $i.sig
         gpg --no-tty --pinentry-mode=loopback --passphrase $PW_GPG --detach-sign --use-agent -u $KEY_GPG --no-armor "$i2"
-        repo-add --verify --sign --key $KEY_GPG $repo.db.tar.gz $i2
+        repo-add $repo.db.tar.gz $i2
         del-old-pkg $i2
+        name_pkg = $(get_name $i)
+        if ! $(echo "$name_pkg" | grep -q '\-static'); then
+          repo-remove $repo.db.tar.gz $name_pkg || true
+          del-all-pkg "${name_pkg}-static"
+        fi
         put-object $repo/$arch/$i2 $i2
         put-object $repo/$arch/$i2.sig $i2.sig
         upload=true
@@ -84,14 +91,14 @@ if $upload; then
       rm $repo.json
       echo "Attention: ${repo}.json was removed because sig didn't match."
     fi
-    rm $repo.json.sig
   fi
   python mrj.py $repo
   mv $repo.json $repo.json.tar.gz
-  gpg --no-tty --pinentry-mode=loopback --passphrase $PW_GPG --detach-sign --use-agent -u $KEY_GPG --no-armor "$repo.json.tar.gz"
 
   # Upload db, sig of db and json of repo
   for i in db files json; do
+    rm $repo.$i.tar.gz.sig
+    gpg --batch --pinentry-mode=loopback --passphrase $PW_GPG --detach-sign --use-agent -u $KEY_GPG --no-armor "$repo.$i.tar.gz"
     put-object $repo/$arch/$repo.$i $repo.$i.tar.gz
     put-object $repo/$arch/$repo.$i.sig $repo.$i.tar.gz.sig
     rm $repo.$i*
