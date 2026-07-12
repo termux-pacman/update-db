0c1c9fe39d428c07d5963eb6199475b8b2f68228
Max Ivan <mixython@gmail.com>
2026-07-12 18:54:53 +0300

    add invalidation (#4)

--- main.sh
+++ main.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-repo="$1"
+repo="${1}"
 bucket="termux-pacman.us"
-arch="$2"
+arch="${2}"
 upload=false
 oldpkgs="oldpkgs.db"
 update_oldpkgs=false
@@ -30,7 +30,7 @@ error() {
 get_name() {
 	local name="" file_sp=(${1//-/ })
 	for _index_f_get_name in $(seq 0 $((${#file_sp[*]}-4))); do
-		if [ -z $name ]; then
+		if [ -z ${name} ]; then
 			name="${file_sp[_index_f_get_name]}"
 		else
 			name+="-${file_sp[_index_f_get_name]}"
@@ -41,7 +41,7 @@ get_name() {
 
 get-object() {
 	info "aws s3api get-object --bucket ${bucket} --key ${1} ${2}"
-	aws s3api get-object --bucket ${bucket} --key ${1} ${2}
+	aws s3api get-object --bucket "${bucket}" --key "${1}" "${2}"
 	echo -e "\n\n"
 }
 
@@ -60,23 +60,31 @@ put-object() {
 aws-rm() {
 	info "aws s3 rm s3://${bucket}/${1}"
 	if [ "${GITHUB_EVENT}" != "pull_request" ]; then
-		aws s3 rm s3://${bucket}/${1}
+		aws s3 rm "s3://${bucket}/${1}"
+	fi
+	echo -e "\n\n"
+}
+
+invalidation() {
+	info "aws cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --paths /${1}"
+	if [ "${GITHUB_EVENT}" != "pull_request" ]; then
+		aws cloudfront create-invalidation --distribution-id "${DISTRIBUTION_ID}" --paths "/${1}"
 	fi
 	echo -e "\n\n"
 }
 
 ls_files_s3() {
 	local request=$(aws s3api list-objects --bucket "${1}" --prefix "${2}")
-	if [[ $(jq -r '."Contents"' <<< "$request") != "null" ]]; then
-		jq -r '.Contents[].Key' <<< "$request"
+	if [[ $(jq -r '."Contents"' <<< "${request}") != "null" ]]; then
+		jq -r '.Contents[].Key' <<< "${request}"
 	fi
 }
 
 del-old-pkg() {
-	local name_pkg=$(get_name $1)
-	for _pkg_f_del_old_pkg in $(grep "/${name_pkg}-" <<< "$files"); do
-		if [[ ${_pkg_f_del_old_pkg} != *".pkg."*".sig" && $1 != $(sed 's/+/0/g' <<< ${_pkg_f_del_old_pkg##*/}) && \
-			$name_pkg = $(get_name ${_pkg_f_del_old_pkg##*/}) ]] && \
+	local name_pkg=$(get_name "${1}")
+	for _pkg_f_del_old_pkg in $(grep "/${name_pkg}-" <<< "${files}"); do
+		if [[ "${_pkg_f_del_old_pkg}" != *".pkg."*".sig" && "${1}" != $(sed 's/+/0/g' <<< "${_pkg_f_del_old_pkg##*/}") && \
+			"${name_pkg}" = $(get_name "${_pkg_f_del_old_pkg##*/}") ]] && \
 			! grep -q "^${_pkg_f_del_old_pkg}===" ${oldpkgs}; then
 			echo "${_pkg_f_del_old_pkg}===$(date +%s)" >> ${oldpkgs}
 		fi
@@ -85,8 +93,8 @@ del-old-pkg() {
 }
 
 del-all-pkg() {
-	for _pkg_f_del_all_pkg in $(grep "/${1}-" <<< "$files"); do
-		if [[ ${_pkg_f_del_all_pkg} != *".pkg."*".sig" && $1 = $(get_name ${_pkg_f_del_all_pkg##*/}) ]] && \
+	for _pkg_f_del_all_pkg in $(grep "/${1}-" <<< "${files}"); do
+		if [[ "${_pkg_f_del_all_pkg}" != *".pkg."*".sig" && "${1}" = $(get_name "${_pkg_f_del_all_pkg##*/}") ]] && \
 			! grep -q "^${_pkg_f_del_all_pkg}===" ${oldpkgs}; then
 			echo "${_pkg_f_del_all_pkg}===$(date +%s)" >> ${oldpkgs}
 		fi
@@ -96,9 +104,9 @@ del-all-pkg() {
 
 # Get and check dbs
 for format in db files; do
-	get-object $repo/$arch/$repo.$format $repo.$format.tar.gz
-	get-object $repo/$arch/$repo.$format.sig $repo.$format.tar.gz.sig
-	if ! gpg --verify $repo.$format.tar.gz.sig $repo.$format.tar.gz; then
+	get-object "${repo}/${arch}/${repo}.${format}" "${repo}.${format}.tar.gz"
+	get-object "${repo}/${arch}/${repo}.${format}.sig" "${repo}.${format}.tar.gz.sig"
+	if ! gpg --verify "${repo}.${format}.tar.gz"{.sig,}; then
 		error "invalid signature"
 	fi
 done
@@ -108,98 +116,98 @@ files=$(ls_files_s3 "${bucket}" "${repo}/${arch}/")
 sfpu_files=$(ls_files_s3 "${SFPU}" "${repo}/${arch}/")
 
 # Delete old packages
-bucket="$SFPU" get-object system-files/$repo/$arch/$oldpkgs $oldpkgs
-for i in $(cat $oldpkgs); do
+bucket="${SFPU}" get-object "system-files/${repo}/${arch}/${oldpkgs}" "${oldpkgs}"
+for i in $(cat ${oldpkgs}); do
 	if (( (($(date +%s) - ${i#*===}) / 3600 ) > 12 )); then
-		for j in $(grep ${i%%===*} <<< "$files"); do
-			aws-rm $j
+		for j in $(grep "${i%%===*}" <<< "${files}"); do
+			aws-rm "${j}"
 		done
-		sed -i "\|^${i}$|d" $oldpkgs
+		sed -i "\|^${i}$|d" ${oldpkgs}
 		update_oldpkgs=true
 	fi
 done
 
 # Delete packages and sig of packages
-case $repo in
+case ${repo} in
 	main|root|x11) name_fdp="deleted_termux-${repo}_packages.txt";;
 	*) name_fdp="deleted_${repo}_packages.txt";;
 esac
-files_dp=$(grep "$name_fdp" <<< "$sfpu_files" | head -1)
-if [[ -n $files_dp ]]; then
-	for i in $files_dp; do
-		if [[ $i != *"$name_fdp"*".sig" ]]; then
-			bucket="$SFPU" get-object $i $name_fdp
-			bucket="$SFPU" get-object $i.sig $name_fdp.sig
-			if gpg --verify $name_fdp.sig $name_fdp; then
-				for j in $(cat $name_fdp); do
-					for z in "" "-static"; do
-						repo-remove $repo.db.tar.gz "${j}${z}" || true
-						del-all-pkg $(echo "${j}${z}" | sed 's/+/0/g')
-					done
+files_dp=$(grep "${name_fdp}" <<< "${sfpu_files}") || true
+for i in ${files_dp}; do
+	if [[ "${i}" != *"${name_fdp}"*".sig" ]]; then
+		bucket="${SFPU}" get-object "${i}" "${name_fdp}"
+		bucket="${SFPU}" get-object "${i}.sig" "${name_fdp}.sig"
+		if gpg --verify "${name_fdp}"{.sig,}; then
+			for j in $(cat "${name_fdp}"); do
+				for z in "" "-static"; do
+					repo-remove "${repo}.db.tar.gz" "${j}${z}" || true
+					del-all-pkg "${j//+/0}${z}"
 				done
-				upload=true
-			else
-				warn "package removal failed, invalid signature"
-			fi
-			rm $name_fdp*
+			done
+			upload=true
+		else
+			warn "package removal failed, invalid signature"
 		fi
-	done
-fi
+		rm "${name_fdp}"*
+	fi
+done
 
 # Update packages and create new sigs of packages
-files_pkg=$(grep "\.pkg\." <<< "$sfpu_files") || true
-if [[ -n $files_pkg ]]; then
-	for i in $files_pkg; do
-		if [[ $i != *".pkg."*".sig" ]]; then
-			i2=$(sed 's/+/0/g' <<< ${i##*/})
-			bucket="$SFPU" get-object $i $i2
-			bucket="$SFPU" get-object $i.sig $i2.sig
-			if gpg --verify $i2.sig $i2; then
-				rm $i2.sig
-				gpg --no-tty --pinentry-mode=loopback --passphrase $PW_GPG --detach-sign --use-agent -u $KEY_GPG --no-armor "$i2"
-				repo-add $repo.db.tar.gz $i2
-				del-old-pkg $i2
-				name_pkg=$(get_name $i2)
-				if ! grep -q '\-static' <<< "$name_pkg"; then
-					repo-remove $repo.db.tar.gz "${name_pkg}-static" || true
-					del-all-pkg "${name_pkg}-static"
-				elif grep -q "^${repo}/${arch}/${i2}===" ${oldpkgs}; then
-					sed -i "\|^${repo}/${arch}/${i2}===|d" ${oldpkgs}
-				fi
-				put-object $repo/$arch/$i2 $i2
-				put-object $repo/$arch/$i2.sig $i2.sig
-				upload=true
-			else
-				warn "failed to update package '${i}', invalid signature"
+files_pkg=$(grep "\.pkg\." <<< "${sfpu_files}") || true
+for i in ${files_pkg}; do
+	if [[ "${i}" != *".pkg."*".sig" ]]; then
+		i2=$(sed 's/+/0/g' <<< "${i##*/}")
+		bucket="${SFPU}" get-object "${i}" "${i2}"
+		bucket="${SFPU}" get-object "${i}.sig" "${i2}.sig"
+		if gpg --verify "${i2}"{.sig,}; then
+			rm ${i2}.sig
+			gpg --no-tty --pinentry-mode=loopback --passphrase "${PW_GPG}" --detach-sign --use-agent -u "${KEY_GPG}" --no-armor "${i2}"
+			repo-add "${repo}.db.tar.gz" "${i2}"
+			del-old-pkg "${i2}"
+			name_pkg=$(get_name "${i2}")
+			if ! grep -q '\-static' <<< "${name_pkg}"; then
+				repo-remove "${repo}.db.tar.gz" "${name_pkg}-static" || true
+				del-all-pkg "${name_pkg}-static"
+			elif grep -q "^${repo}/${arch}/${i2}===" ${oldpkgs}; then
+				sed -i "\|^${repo}/${arch}/${i2}===|d" ${oldpkgs}
 			fi
-			rm $i2*
+			for j in "" ".sig"; do
+				put-object "${repo}/${arch}/${i2}${j}" "${i2}${j}"
+				if grep -q "/${i2}${j}$" <<< "${files}"; then
+					invalidation "${repo}/${arch}/${i2}${j}"
+				fi
+			done
+			upload=true
+		else
+			warn "failed to update package '${i}', invalid signature"
 		fi
-	done
-fi
+		rm ${i2}*
+	fi
+done
 
 # Upload list old pkgs
-if $update_oldpkgs; then
-	bucket="$SFPU" put-object system-files/$repo/$arch/$oldpkgs $oldpkgs
+if ${update_oldpkgs}; then
+	bucket="${SFPU}" put-object "system-files/${repo}/${arch}/${oldpkgs}" "${oldpkgs}"
 	export UPDATE_RSYNC=true
 fi
 
-if $upload; then
+if ${upload}; then
 	# Create json of repo
-	python mrj.py $repo
-	mv $repo.json $repo.json.tar.gz
+	python mrj.py ${repo}
+	mv ${repo}.json ${repo}.json.tar.gz
 
 	# Upload db, sig of db and json of repo
 	disable_cache_policy=true
 	for i in db files json; do
-		rm $repo.$i.tar.gz.sig || true
-		gpg --batch --pinentry-mode=loopback --passphrase $PW_GPG --detach-sign --use-agent -u $KEY_GPG --no-armor "$repo.$i.tar.gz"
-		put-object $repo/$arch/$repo.$i $repo.$i.tar.gz
-		put-object $repo/$arch/$repo.$i.sig $repo.$i.tar.gz.sig
+		rm ${repo}.${i}.tar.gz.sig || true
+		gpg --batch --pinentry-mode=loopback --passphrase "${PW_GPG}" --detach-sign --use-agent -u "${KEY_GPG}" --no-armor "${repo}.${i}.tar.gz"
+		put-object "${repo}/${arch}/${repo}.${i}" "${repo}.${i}.tar.gz"
+		put-object "${repo}/${arch}/${repo}.${i}.sig" "${repo}.${i}.tar.gz.sig"
 	done
 
 	# Removing files from SFPU
-	for i in $sfpu_files; do
-		bucket="$SFPU" aws-rm $i
+	for i in ${sfpu_files}; do
+		bucket="${SFPU}" aws-rm "${i}"
 	done
 	export UPDATE_RSYNC=true
 fi
